fb70a978414b98e3bc66d029bd7ac4eb6dc6f4e1
Rishabh Sagar <rishabh123sagar@gmail.com>
2021-06-23 19:34:55 +0530

    Update antivirus.bat

--- antivirus.bat
+++ antivirus.bat
@@ -4,12 +4,12 @@ mode 1000
 :start
 cls
 echo scanning start
-IF EXIST virusfile.bat goto infected
+IF EXIST virus.bat goto infected
 IF NOT EXIST goto clean
 :infected
 echo If found then delete
 echo press any key to delete
-del virusfile.bat
+del virus.bat
 taskkill /f /im wscript.exe
 pause>nul
 goto start
