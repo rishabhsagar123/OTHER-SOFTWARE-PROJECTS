456d3f74e14c96aa0a76eff9b7668277de74e212
Rishabh Sagar <rishabh123sagar@gmail.com>
2021-12-04 10:15:14 +0530

    Update antivirusadvance.bat

--- antivirusadvance.bat
+++ antivirusadvance.bat
@@ -1,5 +1,5 @@
 @echo off
-title antivi
+title The Shield
 mode 1000
 color 0a
 goto start
@@ -11,6 +11,14 @@ if exist .vbs goto b
 color 0c
 
 echo Something .bat file is Running In Your Pc, Wait We Solved it!!
+pause
+color 06
+echo We Make A search.log File For Currently Running Process..
+pause
+echo Total Running Processes
+tasklist /v /fi "STATUS eq running"
+pause
+tasklist /FI "IMAGENAME eq cmd.exe" /FO CSV > search.log
 pause
 color 0a
 echo Your System is Now Safe!!
@@ -27,7 +35,14 @@ goto start
 color 0b
 
 Something .vbs file is Running In Your Pc, Wait We Solved it!!
-
+pause
+color 06
+echo We Make A search.log File For Currently Running Process..
+pause
+echo Total Running Processes
+tasklist /v /fi "STATUS eq running"
+pause
+tasklist /FI "IMAGENAME eq wscript.exe" /FO CSV > search.log
 pause
 color 0a
 echo Your System is Now Safe!!
