846563ad4083e35023a6332eb127f828291c3145
Rishabh Sagar <rishabh123sagar@gmail.com>
2022-01-07 20:01:59 +0530

    Update webcrawler.js

--- webcrawler.js
+++ webcrawler.js
@@ -69,3 +69,87 @@ function collectInternalLinks($) {
         pagesToVisit.push(baseUrl + $(this).attr('href'));
     });
 }
+
+------------------------------------------------------------------------------------For a Link----------------------------------------------------------
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+var URLZ= require('url-parse');
+
+var START_URL = "https://passportindia.gov.in/AppOnlineProject/welcomeLink";
+var MAX_PAGES_TO_VISIT = 10;
+var SEARCH_WORD="https://passportindia.gov.in";
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var urlz = new URLZ(SEARCH_WORD);
+var baseUrl = url.protocol + "//" + url.hostname;
+var basicurl= urlz.protocol + "//" + urlz.hostname;
+
+pagesToVisit.push(START_URL);
+
+crawl();
+
+function crawl() {
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  var nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  } else {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
+
+function visitPage(url, callback) {
+  // Add page to our set
+  pagesVisited[url] = true;
+  numPagesVisited++;
+
+  // Make the request
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Parse the document body
+     var $ = cheerio.load(body);
+     var isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
+
+function searchForWord($, word) {
+   // var urlRegex =/https:\/\/passportindia\.gov\.in/;
+    var bodyText = $('html > body').text().toLowerCase();
+   // return bodyText.replace(urlRegex, function(url) {
+  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+     
+}
+  
+ 
+
+
+function collectInternalLinks($) {
+    var relativeLinks = $("a[href^='/']");
+    console.log("Found " + relativeLinks.length + " relative links on page");
+    relativeLinks.each(function() {
+      if(basicurl.search(baseUrl)!==-1){
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
+      }
+    });
+}
